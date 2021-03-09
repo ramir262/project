3adf7961a787a21a0c00f2ad52087b2776dd96e1
allis <allis@AllisonPC>
2021-03-08 19:57:51 -0800

    created thread for database insert (account creation)

--- PantherInspectProject/src/pantherinspectproject/SignupForm.java
+++ PantherInspectProject/src/pantherinspectproject/SignupForm.java
@@ -99,10 +99,9 @@ public class SignupForm {
             //run account creation
         try {
             String aid = master.qp.getAccountId(email.getText());
-            System.out.println(aid);
             if (aid.equals("0")) {
-                createSignupEvent(email.getText(),username.getText(),pwBox.getText(),gradyear.getText(),gradsemester.getText());
-                
+                Thread thr = new Thread(() -> createSignupEvent(email.getText(),username.getText(),pwBox.getText(),gradyear.getText(),gradsemester.getText()));
+                thr.start();
                 //return to initial scene
                 master.start(primaryStage);
             }
