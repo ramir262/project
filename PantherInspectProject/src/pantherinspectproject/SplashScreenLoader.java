890a976d11dc12eb7a865f3c7b2e1df7a95326fb
allis <allis@AllisonPC>
2021-03-26 22:17:44 -0700

    removed border from splash screen SplashScreenLoader.java

--- PantherInspectProject/src/pantherinspectproject/SplashScreenLoader.java
+++ PantherInspectProject/src/pantherinspectproject/SplashScreenLoader.java
@@ -14,6 +14,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -26,6 +27,7 @@ public class SplashScreenLoader extends Preloader {
     public void start(Stage stage) throws Exception {
         splashScreen = stage;
         splashScreen.setScene(createSplash());
+        stage.initStyle(StageStyle.UNDECORATED);
         splashScreen.show();
     }
     /*
